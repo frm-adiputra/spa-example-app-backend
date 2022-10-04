9c50ac6e5eeba4c4b9dfd751d3adce67fd60faef
Firmansyah Adiputra <frm.adiputra@gmail.com>
2022-10-04 21:20:10 +0700

    fix: authentication

--- src/authentication.js
+++ src/authentication.js
@@ -1,13 +1,42 @@
-const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
+const { OAuthStrategy, expressOauth } = require('@feathersjs/authentication-oauth');
+const { AuthenticationService } = require('@feathersjs/authentication');
 
-const { expressOauth } = require('@feathersjs/authentication-oauth');
+class GoogleStrategy extends OAuthStrategy {
+  async getEntityData(profile) {
+  
+    // this will set 'googleId'
+    const baseData = await super.getEntityData(profile);
+    
+    // this will grab the picture and email address of the Google profile
+    return {
+      ...baseData,
+      // profilePicture: profile.picture,
+      email: profile.email
+    };
+  }
+}
 
 module.exports = app => {
   const authentication = new AuthenticationService(app);
 
-  authentication.register('jwt', new JWTStrategy());
-
+  // authentication.register('jwt', new JWTStrategy());
+  // authentication.register('local', new LocalStrategy());
+  authentication.register('google', new GoogleStrategy());
 
   app.use('/authentication', authentication);
   app.configure(expressOauth());
 };
+
+// const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
+
+// const { expressOauth } = require('@feathersjs/authentication-oauth');
+
+// module.exports = app => {
+//   const authentication = new AuthenticationService(app);
+
+//   authentication.register('jwt', new JWTStrategy());
+
+
+//   app.use('/authentication', authentication);
+//   app.configure(expressOauth());
+// };
--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -1,36 +1,49 @@
-const { authenticate } = require('@feathersjs/authentication').hooks;
+const { authenticate } = require("@feathersjs/authentication").hooks;
 
+async function logData(context) {
+  console.log("-->", context.data);
+}
 
+async function logType(context) {
+  console.log("==>", context.type, context.method);
+}
 
+async function logError(context) {
+  console.error(context.error.message)
+}
+
+async function logResult(context) {
+  console.log('--> RESULT')
+  console.log(context.result)
+}
 module.exports = {
   before: {
-    all: [],
-    find: [ authenticate('jwt') ],
-    get: [ authenticate('jwt') ],
-    create: [  ],
-    update: [  authenticate('jwt') ],
-    patch: [  authenticate('jwt') ],
-    remove: [ authenticate('jwt') ]
+    all: [logType],
+    find: [authenticate("jwt")],
+    get: [authenticate("jwt")],
+    create: [logData],
+    update: [authenticate("jwt")],
+    patch: [authenticate("jwt")],
+    remove: [authenticate("jwt")],
   },
 
   after: {
-    all: [ 
-    ],
-    find: [],
+    all: [],
+    find: [logResult],
     get: [],
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   error: {
-    all: [],
+    all: [logError],
     find: [],
     get: [],
     create: [],
     update: [],
     patch: [],
-    remove: []
-  }
+    remove: [],
+  },
 };
